24d4d5f50e2845ea8db60537d6cd7ff45c026d73
Michael Davidsaver <mdavidsaver@gmail.com>
2024-09-07 19:02:51 -0700

    update build

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ import os
 from pathlib import Path
 import platform
 import sysconfig
-from distutils.log import info, warn
-from distutils.spawn import find_executable
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-
-from numpy.distutils.misc_util import get_numpy_include_dirs
+try:
+    from shutil import which as find_executable
+except ImportError:
+    from distutils.spawn import find_executable
 
 from Cython.Build import cythonize
 
@@ -25,7 +25,12 @@ def env_path(name):
         return []
 
 include_dirs = env_path('AACLIENT_INCLUDE_DIRS')
-include_dirs += get_numpy_include_dirs()
+try:
+    from numpy import get_include
+    include_dirs.append(get_include())
+except ImportError:
+    from numpy.distutils.misc_util import get_numpy_include_dirs
+    include_dirs += get_numpy_include_dirs()
 include_dirs.append('aaclient') # aaclient/pb.h
 
 library_dirs = env_path('AACLIENT_LIB_DIRS')
@@ -70,7 +75,7 @@ class BuildProtoC(build_ext):
                 # final source name
                 src = tmp.with_suffix('.cpp') # "build/temp.*/EPICSEvent.pb.cpp"
 
-                info("GEN protoc {!r} -> {!r}".format(proto, src))
+                print("GEN protoc {!r} -> {!r}".format(proto, src))
 
                 if not self.protoc:
                     raise RuntimeError("Failed to find protoc executable in %s"%self.__path)
@@ -78,10 +83,10 @@ class BuildProtoC(build_ext):
                 self.spawn([self.protoc, '--cpp_out='+str(self.protoout), proto])
 
                 if not src.exists() or src.read_bytes()!=tmp.read_bytes():
-                    info("UPDATED {!r}".format(src))
+                    print("UPDATED {!r}".format(src))
                     self.copy_file(tmp, src)
                 else:
-                    info("UNCHANGED {!r}".format(src))
+                    print("UNCHANGED {!r}".format(src))
 
             new_srcs.append(str(src))
 
@@ -98,17 +103,17 @@ class BuildProtoC(build_ext):
             # Copy in protobuf-lite dll
             dll=protobuf_lite+'.dll'
             dest=os.path.join(self.build_lib, 'aaclient', dll)
-            info('Searching for %r in %r', dll, self.__path)
+            print('Searching for %r in %r', dll, self.__path)
             for dname in self.__path.split(os.pathsep):
                 if not dname or not os.path.isdir(dname):
                     continue
                 cand = os.path.join(dname, dll)
                 if os.path.isfile(cand):
-                    info('Copying %r -> %r', cand, dest)
+                    print('Copying %r -> %r', cand, dest)
                     self.copy_file(cand, dest)
                     break
             else:
-                warn("Did not find %r", dll)
+                print("WARNING: Did not find %r", dll)
 
 
 BuildProtoC.user_options = build_ext.user_options + [
